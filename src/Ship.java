c65fa922f4ad14982203e1768ace771b78309282
luism <luis.couto@mindswap.academy>
2022-05-30 14:43:52 +0100

    skeleton

---
+++ src/Ship.java
@@ -0,0 +1,11 @@
+public class Ship {
+    private int size;
+    private String axis;
+    private Position position;
+
+    public Ship(int size, String axis, Position position) {
+        this.size = size;
+        this.axis = axis;
+        this.position = position;
+    }
+}
---
+++ src/ShipsENUM.java
@@ -0,0 +1,6 @@
+public enum ShipsENUM {
+    TWO_BLOCK_SHIP,
+    THREE_BLOCK_SHIP,
+    FOUR_BLOCK_SHIP,
+    FIVE_BLOCK_SHIP;
+}
