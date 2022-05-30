5319d27834d01300a911879cfffc83b6d55f2862
Nuno Carmo <ok>
2022-05-30 18:08:00 +0100

    commit

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,25 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Player {
 
     private String name;
     //score??
+
+//    public static void main(String[] args) {
+//        Client a = new Client();
+//        try {
+//            a.start("localHost", 8082);
+//        } catch (IOException e) {
+//            System.out.println("Chat is under maintenance");
+//        }
+//    }
+//    public void start(String host, int port) throws IOException {
+//        Socket socket = new Socket(host, port);
+//        new Thread(new KeyboardHandler(socket)).start();
+//    }
+
+
 
 
     public Player(String name) {
