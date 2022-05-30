14a0431c5fb8c7954a9428107a3b84d0a2f7e2f1
luism <luis.couto@mindswap.academy>
2022-05-30 15:00:24 +0100

    skeleton

--- src/colors/Colors.java
+++ src/colors/Colors.java
@@ -6,7 +6,7 @@ public enum Colors {
     // Regular Colors. Normal color, no bold, background color etc.
 //    BLACK("\033[0;30m"),    // BLACK
     RED("\033[0;31m"),      // RED
-    //    GREEN("\033[0;32m"),    // GREEN
+    GREEN("\033[0;32m"),    // GREEN
     YELLOW("\033[0;33m"),   // YELLOW
     //    BLUE("\033[0;34m"),     // BLUE
 //    MAGENTA("\033[0;35m"),  // MAGENTA
---
+++ src/commands/QuitHandler.java
@@ -0,0 +1,4 @@
+package commands;
+
+public class QuitHandler implements CommandHandler{
+}
