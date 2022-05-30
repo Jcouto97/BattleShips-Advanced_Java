9c0b2070447940570d29bd90382169b4c9496b22
Nuno Carmo <nuno.carmo@mindswap.academy>
2022-05-30 22:06:02 +0100

    board drawn with 1 fixed ship

--- src/field/Board.java
+++ src/field/Board.java
@@ -2,12 +2,39 @@ package field;
 
 import gameobjects.Ship;
 
-public class Board {
-    private Position[][] position;
+import java.util.Arrays;
 
-    public Board(Position[][] position) {
-        this.position = new Position[10][10]; // mudar para constantes
+public class Board {
+    private String[][] board;
+
+    public Board() {
+        this.board = new String[10][10]; // mudar para constantes
+        int numberOfRows = 1;
+        int numberOfCols = 1;
+        drawNumbersAndWater(numberOfRows, numberOfCols);
         addShip();
+    }
+
+    private void drawNumbersAndWater(int numberOfRows, int numberOfCols) {
+        for (int i = 0; i < this.board.length; i++) {
+            for (int i1 = 0; i1 < this.board[i].length; i1++) {
+                if(i==0 && i1 == 0){
+                    this.board[i][i1] = " ";
+                    continue;
+                }
+                if(i == 0){
+                    this.board[i][i1] = String.valueOf(numberOfRows);
+                    numberOfRows++;
+                    continue;
+                }
+                if(i1 == 0){
+                    this.board[i][i1] = String.valueOf(numberOfCols);
+                    numberOfCols++;
+                    continue;
+                }
+                this.board[i][i1] = "~";
+            }
+        }
     }
 
     // NUNO
@@ -15,5 +42,23 @@ public class Board {
         //buscar barcos aos enums
         // 1 de 5 blocos, 1 de 4, 2 de 3, 1 de 2
         //Ship ship = new Ship(1,"x", new Position(1,2)); mudar 1 e 2 para random
+        Ship ship = new Ship(1,new Position(5,5));
+        this.board[ship.getPosition().getX()][ship.getPosition().getY()] = "#";
+
     }
+
+    public String[][] getBoard() {
+        return board;
+    }
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        for (int i = 0; i < b.getBoard().length; i++) {
+            for (int i1 = 0; i1 < b.getBoard()[i].length; i1++) {
+                System.out.print(" "+b.getBoard()[i][i1]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
 }
--- src/gameobjects/Ship.java
+++ src/gameobjects/Ship.java
@@ -4,12 +4,16 @@ import field.Position;
 
 public class Ship {
     private int size;
-    private String axis;
+ //   private String axis;
     private Position position;
 
-    public Ship(int size, String axis, Position position) {
+    public Ship(int size, Position position) {
         this.size = size;
-        this.axis = axis;
+       // this.axis = axis;
         this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
     }
 }
